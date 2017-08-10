c866305d6de52aa04e3764d714d159b5e667b1ee
cstapiarubio <cstapiarubio@gmail.com>
2017-08-10 10:35:41 -0400

    primer commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+/*------ JS ------*/
+gulp.task('script', function(){
+	gulp.src(['node_modules/jquery/dist/jquery.js','assets/js/*.js'])
+		.pipe(concat('script.js'))
+		//carpeta dist
+		.pipe(gulp.dest('dist/js/'));
+});
+/*------ Estilos ------*/
+gulp.task('style',function(){
+	gulp.src('assets/sass/main.scss')
+		.pipe(sass().on('error',sass.logError))
+		.pipe(minifyCSS())
+		.pipe(concat('style.min.css'))
+		.pipe(gulp.dest('dist/css/'));
+});
+/*------ Watch Sass ------*/
+gulp.task('watch', function(){
+	gulp.watch('assets/sass/*.scss',['style']);
+});
+/*------ Llamada ------*/
+gulp.task('default', ['script','style','watch']);
+
+
+
+
